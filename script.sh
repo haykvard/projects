cbb2ba2deb85b68f248b7b5038bb2b4174548117
Hayk Vardanyan <hefvwjhe@mail.com>
2020-05-10 13:20:40 +0400

    Change script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
-Name=$1
+FirstName=$1
 SecondName=$2
 LastName=$3
 Show=$4
 
 if [ "$Show" = "true" ]; then
-  echo "Hello, $Name $SecondName $LastName"
+  echo "Hello, $FirstName $SecondName $LastName"
 else
   echo "If you want to see the name, please mark the show option"
 fi
